5443ede4eabc57ad40ba7389047205963334fc03
hongjinn <hongjinnpark@gmail.com>
2020-12-26 17:19:46 -0800

    added dockerfile for django_gunicorn and also for nginx

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wk8akj2ygh5!=$#r@_m4peahmyf)y==_ylc8!qxmv&^t_9fb-v'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG")
 
 ALLOWED_HOSTS = ['localhost', '101.42.69.777', 'www.example.com']
 
